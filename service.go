b4565147736e08ce769e09d4dd0f6f86cc32842d
Daniel Kertesz <daniel@spatof.org>
2019-03-13 18:12:27 +0000

    JSON support

--- service.go
+++ service.go
@@ -1,6 +1,7 @@
 package prcdapi
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,23 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
+func respondJSON(w http.ResponseWriter, payload interface{}, code int) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(code)
+		w.Write([]byte(response))
+	}
+}
+
+func respondJSONError(w http.ResponseWriter, message string, code int) {
+	respondJSON(w, map[string]string{"error": message}, code)
+}
 
 // Server expose the PRCD API via HTTP.
 type Server struct {
@@ -18,10 +36,20 @@ type Server struct {
 func NewServer(g *Grimoire) *Server {
 	s := &Server{db: g}
 	r := mux.NewRouter()
+
+	rJSON := r.Headers("Content-Type", "application/json").Subrouter()
+	rJSON.HandleFunc("/prcd", s.prcdRandomJSONHandler())
+	rJSON.HandleFunc("/prcd/{section}", s.prcdJSONHandler())
+
+	rJSONa := r.Headers("Accept", "application/json").Subrouter()
+	rJSONa.HandleFunc("/prcd", s.prcdRandomJSONHandler())
+	rJSONa.HandleFunc("/prcd/{section}", s.prcdJSONHandler())
+
 	r.HandleFunc("/prcd/{section}", s.prcdHandler())
 	r.HandleFunc("/prcd", s.prcdRandomHandler())
 	r.HandleFunc("/sections", s.prcdSectionsHandler())
 	r.HandleFunc("/", s.prcdRandomHandler())
+
 	http.Handle("/", r)
 
 	return s
@@ -41,11 +69,23 @@ func (s *Server) prcdRandomHandler() http.HandlerFunc {
 		moccolo, err := s.db.FromRandomSection()
 		if err != nil {
 			log.Print(err)
-			http.Error(w, "Internal error", http.StatusInternalServerError)
+			http.Error(w, "Internal error: fetching moccolo.", http.StatusInternalServerError)
 		} else {
 			if _, err := w.Write([]byte(fmt.Sprintf("%s (%s)\n", moccolo.Text, moccolo.Author))); err != nil {
 				log.Print(err)
 			}
+		}
+	}
+}
+
+func (s *Server) prcdRandomJSONHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		moccolo, err := s.db.FromRandomSection()
+		if err != nil {
+			log.Print(err)
+			respondJSONError(w, "Error fetching a random Moccolo.", http.StatusInternalServerError)
+		} else {
+			respondJSON(w, moccolo, 200)
 		}
 	}
 }
@@ -55,18 +95,37 @@ func (s *Server) prcdHandler() http.HandlerFunc {
 		vars := mux.Vars(r)
 		sectionName, ok := vars["section"]
 		if !ok {
-			http.Error(w, "Internal error: Section not found", http.StatusInternalServerError)
+			http.Error(w, "Internal error: Section not found.", http.StatusInternalServerError)
 			return
 		}
 
 		moccolo, err := s.db.FromSection(sectionName)
 		if err != nil {
 			log.Print(err)
-			http.Error(w, "Internal error", http.StatusInternalServerError)
+			http.Error(w, "Internal error: fetching moccolo from section.", http.StatusInternalServerError)
 		} else {
 			if _, err := w.Write([]byte(fmt.Sprintf("%s (%s)\n", moccolo.Text, moccolo.Author))); err != nil {
 				log.Print(err)
 			}
+		}
+	}
+}
+
+func (s *Server) prcdJSONHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		sectionName, ok := vars["section"]
+		if !ok {
+			respondJSONError(w, "Internal error: Section not found.", http.StatusInternalServerError)
+			return
+		}
+
+		moccolo, err := s.db.FromSection(sectionName)
+		if err != nil {
+			log.Print(err)
+			respondJSONError(w, "Internal error: fetching moccolo from section.", http.StatusInternalServerError)
+		} else {
+			respondJSON(w, moccolo, 200)
 		}
 	}
 }
